f4220edda9d6e51b5e0f5ba860badd029eed100a
Alkis Gotovos <alkis.gotovos@gmail.com>
2024-01-26 17:50:12 +0100

    Initial setup

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,38 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss'
 
 const config: Config = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
+    colors: {
+      white: '#FFFFFF',
+      background: '#070724',
+      grayishBlue: '#38384F',
+      lightGrayBlue: '#838391',
+      skyBlue: '#419EBB',
+      sandyOrange: '#EDA249',
+      purple: '#6F2ED6',
+      redOrange: '#D14C32',
+      brightRed: '#D83A34',
+      darkOrange: '#CD5120',
+      teal: '#1EC2A4',
+      brightBlue: '#2D68F0',
+    },
+    fontFamily: {
+      heading: ['var(--font-heading)'],
+      body: ['var(--font-body)'],
+    },
+    screens: {
+      tablet: '640px',
+      desktop: '1280px',
     },
   },
   plugins: [],
-};
-export default config;
+  future: {
+    hoverOnlyWhenSupported: true,
+  },
+}
+export default config
